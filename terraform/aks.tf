910c4f8aa9b9dcff05368adb11e2fcbd3ecb982c
Rajeev Singh <rajeev.singh@nagarro.com>
2026-06-01 23:09:56 +0530

    Add Terraform infrastructure code

---
+++ terraform/aks.tf
@@ -0,0 +1,40 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  dns_prefix          = "${var.project_name}-aks"
+
+  default_node_pool {
+    name                 = "system"
+    vm_size              = "Standard_B2s"
+    vnet_subnet_id       = azurerm_subnet.aks_subnet.id
+    auto_scaling_enabled = true
+    min_count            = 1
+    max_count            = 1
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  tags = {
+    project     = "CloudPulse AI"
+    environment = "shared"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
