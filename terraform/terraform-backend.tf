c086daab05cdbdc5bf4cd61fec220c543d512de3
Rajeev Singh <rajeev.singh@nagarro.com>
2026-06-02 10:59:04 +0530

    Configure Terraform remote backend

--- terraform/terraform-backend.tf
+++ terraform/terraform-backend.tf
@@ -4,6 +4,7 @@ terraform {
     storage_account_name = "cloudpulsetfstaterajeev"
     container_name       = "tfstate"
     key                  = "cloudpulse.tfstate"
+    subscription_id      = "e25c0a93-619e-4b0f-8713-ada2c8e89425"
     use_msi              = true
   }
 }
