38e242c7106636639b608ccbab5397ee8365a6fd
Rajeev Singh <rajeev.singh@nagarro.com>
2026-06-09 19:22:27 +0530

    Refactor log analytics into Terraform module

--- terraform/log-analytics.tf
+++ terraform/log-analytics.tf
@@ -1,7 +1,8 @@
-resource "azurerm_log_analytics_workspace" "law" {
+module "log_analytics" {
+  source = "./modules/log-analytics"
+
   name                = "${var.project_name}-logs"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  sku                 = "PerGB2018"
   retention_in_days   = 30
 }
